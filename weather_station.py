fa1221c3785731f135d8448948fa607d74c83c28
lightningWhite <hor12020@byui.edu>
2021-11-08 12:09:15 -0700

    Adjust the brightness threshold

--- weather_station.py
+++ weather_station.py
@@ -45,7 +45,7 @@ IMAGE_DISK_USAGE_THRESHOLD = 1000000000 # 1 GB
 # images will not be saved to the disk. This is so images won't be taken
 # during the night.
 # TODO: This value may need further tuning after deployment and further testing
-BRIGHTNESS_THRESHOLD = 20
+BRIGHTNESS_THRESHOLD = 10
 
 # Issues occur unless only a single camera object is used for the duration of
 # the program.
