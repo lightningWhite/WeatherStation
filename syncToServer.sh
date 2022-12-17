7a8f9262e45d17219af2ba4cecf892399eda72cf
lightningWhite <22121347+lightningWhite@users.noreply.github.com>
2022-12-17 15:11:41 -0700

    Prevent out-dated database backup files from being synced to the server

--- syncToServer.sh
+++ syncToServer.sh
@@ -33,6 +33,10 @@ echo Backup server: $BACKUP_SERVER
 echo Remote backup path: $REMOTE_BACKUP_PATH
 # Export a backup of the influx database
 /home/pi/WeatherStation/influxBackup.sh
-rsync -avh $LOCAL_BACKUP_PATH $BACKUP_USER@$BACKUP_SERVER:$REMOTE_BACKUP_PATH
+# Sync the files to the remote server, except for the database files
+rsync -avh --exclude $LOCAL_BACKUP_PATH/influxBackupPrev --exclude $LOCAL_BACKUP_PATH/influxBackupLatest $LOCAL_BACKUP_PATH $BACKUP_USER@$BACKUP_SERVER:$REMOTE_BACKUP_PATH
+# Sync the latest database backup files, and delete the old database files
+# from the remote
+rsync -avh $LOCAL_BACKUP_PATH/influxBackupLatest $BACKUP_USER@$BACKUP_SERVER:$REMOTE_BACKUP_PATH --delete
 date
 
